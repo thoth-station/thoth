32fc880986cf5a87ad90df99f41f7d678fe81d49
Fridolin Pokorny <fridolin@redhat.com>
2018-02-21 14:39:58 +0100

    Initial project import

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from distutils.command.install import install as DistutilsInstall
+
+
+class ThothInstall(DistutilsInstall):
+    def run(self):
+        raise NotImplementedError("Please use subpackages for interacting with Thoth")
+
+
+setup(
+    name='thoth',
+    version='0.0.0',
+    description='Thoth project',
+    long_description='Thoth project',
+    author='Fridolin Pokorny',
+    author_email='fridolin@redhat.com',
+    license='GPLv2+',
+    packages=['thoth'],
+    zip_safe=False,
+    cmdclass={'install': ThothInstall}
+)
